00ff9af5dd9491c1607101dd9a5e0444ea5617d7
caarlosdamian <dapecarlos@gmail.com>
2017-08-04 11:22:21 -0500

    Michelada
    Proyecto de michelada

---
+++ michelada/inventary/app/controllers/products_controller.rb
@@ -0,0 +1,17 @@
+class ProductsController < ApplicationController
+    def show
+    @product = Product.find(params[:id])
+  end
+    def new
+  end
+    def create
+          @product = Product.new(product_params)
+ 
+  @product.save
+  redirect_to @product
+  end
+    private
+  def product_params
+    params.require(:product).permit(:name, :description,:price)
+  end
+end
